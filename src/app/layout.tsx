0010628191fd3147f832701fe040a44448bf315f
Argedik <gedikas@hotmail.com>
2024-01-13 20:29:39 +0300

    Starter project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,28 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from 'next/font/google';
+import './globals.css';
 import React from 'react';
+import Navbar from '@/components/navbar/Navbar';
+import Footer from '@/components/footer/Footer';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Next App',
-  description: 'Next.js starter app',
-}
+	title: 'Blog',
+	description: 'Next.js starter app',
+};
 
 interface RootLayoutProps {
-    children: React.ReactNode;
+	children: React.ReactNode;
 }
 
-export default function RootLayout({ children } : RootLayoutProps) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+export default function RootLayout({ children }: RootLayoutProps) {
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				<Navbar />
+				{children}
+				<Footer />
+			</body>
+		</html>
+	);
 }
